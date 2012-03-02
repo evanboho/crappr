0263d5da3240e61fbda7913514ab0f5b69082397
Evan Kuchar <evan.kuchar@gmail.com>
2012-03-02 11:03:46 -0600

    fb-2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -210,6 +210,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
+  require "omniauth-facebook"
   config.omniauth :facebook, "126704914122402", "145e75ea370c0de5faee86970b18eea3",
     {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
 
