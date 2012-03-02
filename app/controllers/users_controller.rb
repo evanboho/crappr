263e44c29e05842d5527f3758eaf70819899e71c
Evan Kuchar <evan.kuchar@gmail.com>
2012-03-02 09:17:54 -0600

    fixed edit problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,15 +59,16 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    respond_to do |format|
+    # respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to sign_in(@user), notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        redirect_to @user
+        #format.html { redirect_to sign_in(@user), notice: 'User was successfully updated.' }
+        # format.json { head :no_content }
+      #else
+       # format.html { render action: "edit" }
+        # format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
+    #end
   end
 
   # DELETE /users/1
