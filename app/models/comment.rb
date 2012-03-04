907b6272054131628bdc2a4e6213dab02e103afd
Evan Kuchar <evan.kuchar@gmail.com>
2012-03-04 16:23:27 -0600

    downcase

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   
+  before_save :downcase
+  
   validates :content, :presence => true, :length => {:minimum => 4}
-
+  
+  def downcase
+    self.content = self.content.downcase
+  end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
   
   before_save :downcase
-  
+    
   validates :user_id, :presence => true
   validates :content, :presence => true, :length => { :maximum => 144 }
-
-
+  
   def downcase
     self.content = self.content.downcase
     replace_qs
